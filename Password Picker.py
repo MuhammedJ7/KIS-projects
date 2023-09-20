f1e9b951e066bef5b05dff10ae9debcdd7485b5c
MuhammedJ7 <134181086+MuhammedJ7@users.noreply.github.com>
2023-09-20 23:12:22 +0100

    Create Password Picker.py

---
+++ Password Picker.py
@@ -0,0 +1,14 @@
+"""In this project,
+build a tool that makes secure, memorable passwords
+to help keep your private information safe"""
+
+"""This project will how to use Pythons random module. The program uses random choices from
+groups of adjectives, nouns, numbers, and punctuation
+characters to assemble each password"""
+
+import random 
+import string
+
+
+
+print ('Welcome to your own personal password picker')
