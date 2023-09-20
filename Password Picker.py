1748c038504d4e671de27ecf79193c78c19c5710
MuhammedJ7 <134181086+MuhammedJ7@users.noreply.github.com>
2023-09-20 23:25:14 +0100

    Update Password Picker.py
    removed,duplicate nouns , adejctives was not declared , cleaned up a bit. basic version works

--- Password Picker.py
+++ Password Picker.py
@@ -1,10 +1,8 @@
-"""In this project,
-build a tool that makes secure, memorable passwords
-to help keep your private information safe"""
-
-"""This project will how to use Pythons random module. The program uses random choices from
+"""In this project, build a tool that makes secure, memorable passwords
+to help keep your private information safe,This project will how to use Pythons random module. The program uses random choices from
 groups of adjectives, nouns, numbers, and punctuation
 characters to assemble each password"""
+
 
 import random 
 import string
@@ -13,17 +11,17 @@ FantasyAdjectives = ['mythical','eerie','magic','supernatural', 'dreamlike', 'et
                      'mystical','imaginary','dark', 'wonderful','enigmatic','fabulous','strange','beautiful','perplexing','dazzling',
                      'utopian','proud', 'brave']
 
-nouns = nouns = ["apple", "dog", "cat", "car", "house", "book", "computer", "flower", "friend", "city", "tree", "music",
+nouns = ["apple", "dog", "cat", "car", "house", "book", "computer", "flower", "friend", "city", "tree", "music",
                   "beach", "sun", "river", "mountain", "child", "school", "food", "movie", "job", "money", "time", "heart"]
 
 
 print ('Welcome to your own personal password picker')
 
 
-adjective = random.choice(adjectives)
+adjective = random.choice(FantasyAdjectives )
 noun = random.choice(nouns)
 number = random.randrange(0, 100)
 special_char = random.choice(string.punctuation)
 password = adjective + noun + str(number) + special_char
 
-print('Your new password is: %s' % password)
+print('Your new password is:', password)
