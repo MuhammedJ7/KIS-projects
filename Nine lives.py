f6cf6e69c71a887b3004fbae23e6b87e312e17f6
MuhammedJ7 <134181086+MuhammedJ7@users.noreply.github.com>
2023-09-22 19:44:08 +0100

    Update Nine lives.py
    first steps , i right now its only 5 words but i wanna find a way so that the string length can be anything and the code can adjust itself

--- Nine lives.py
+++ Nine lives.py
@@ -1,2 +1,16 @@
 """, you have to guess the secret word one letter at a time. If your guess is wrong, you lose
 a life."""
+
+import random 
+
+lives = 9
+FantasyAdjectives = ['mythical','eerie','magic','supernatural', 'dreamlike', 'ethereal','fantastical','gothic','mytholgical',
+                     'mystical','imaginary','dark', 'wonderful','enigmatic','fabulous','strange','beautiful','perplexing','dazzling',
+                     'utopian','proud', 'brave']
+words = ['pizza', 'fairy', 'teeth', 'shirt',
+        'otter', 'plane']
+
+secret.word = random.choice(words)
+clue = list('?????')
+
+heart_symbol = u'\u2764'
