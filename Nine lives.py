51ca34b9bd78c19b96a4bcd1574d72ed84b96f62
MuhammedJ7 <134181086+MuhammedJ7@users.noreply.github.com>
2023-09-23 23:44:50 +0100

    main bulk of code
    The function contains a while loop
    that works through the secret word
    one letter at a time, checking whether
    each letter matches the guessed
    letter. The index variable keeps count
    of the current letter as the program
    scans through the word.

--- Nine lives.py
+++ Nine lives.py
@@ -14,3 +14,15 @@ secret.word = random.choice(words)
 clue = list('?????')
 
 heart_symbol = u'\u2764'
+
+guessed_word_correctly = False
+
+"""The main part of the code is a loop that gets a letter from the player and checks if it’s in the secret word. If it is, 
+the code uses a function to update the clue. You’ll make that function, then create the main loop"""
+
+def update_clue(guessed_letter, secret_word, clue):
+    index = 0
+    while index < len(secret_word):
+        if guessed_letter == secret_word[index]:
+            clue[index] = guessed_letter
+        index = index + 1
